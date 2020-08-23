ef9463db73bd5f4fef2dd5ec3687eaaa350d4ef8
Gloria Nwachukwu <gloriaerose@gmail.com>
2020-08-23 19:20:14 +0100

    Contains importerror

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 
-
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
